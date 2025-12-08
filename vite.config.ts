9d58e83048ac23e3e90ed8b0a5c52533b51b8728
anouarhamidi <anouarhamidi2@gmail.com>
2025-12-08 21:53:58 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/siraajv0.1-main/',
   plugins: [react()],
+  base: '/siraajv0.1-main/', // <-- replace with your GitHub repo name
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
